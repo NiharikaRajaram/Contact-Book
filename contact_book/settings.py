bd9d74b0e04aa1df8b9f8c95c35c33d4d0e9ee26
Niharika Rajaram <abhinihar@gmail.com>
2021-12-08 16:24:26 -0600

    Update settings.py

--- contact_book/settings.py
+++ contact_book/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^hq1+8e2mk4m46qk)#i9^5eap0s0p@08b_*s&s+jn%e(88(zku'
+SECRET_KEY = # enter your secret key here
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
